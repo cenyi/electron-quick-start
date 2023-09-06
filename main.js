d35ce737a2dd39c8a605e99da5692dcbf3132ddb
秋水长天 <qiudong@bgi.com>
2023-09-06 15:34:35 +0800

    增加自动重加载代码

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const electronReload = require('electron-reload');
+electronReload(__dirname);
 
 function createWindow () {
   // Create the browser window.
